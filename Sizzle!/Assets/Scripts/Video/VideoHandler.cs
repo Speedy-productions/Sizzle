518b2083ade6aa7dd28e699a40cdd747d2c9bd0c
A00835838 <A00835838@tec.mx>
2025-11-13 22:52:06 -0600

    arregloAudioVideos

--- Sizzle!/Assets/Scripts/Video/VideoHandler.cs
+++ Sizzle!/Assets/Scripts/Video/VideoHandler.cs
@@ -9,10 +9,23 @@ public class VideoHandler : MonoBehaviour
     [SerializeField] public PlayList playList;
     [SerializeField] int videoIndex = 0;
 
-
     // Start is called before the first frame update
     void Start()
     {
+        // 🔇 Desactivar audio del VideoPlayer
+        if (videoPlayer != null)
+        {
+            // Opción 1: sin salida de audio
+            videoPlayer.audioOutputMode = VideoAudioOutputMode.None;
+
+            // (Opcional) por si en algún momento cambias el modo a Direct,
+            // dejamos todas las pistas en mute.
+            for (ushort i = 0; i < videoPlayer.audioTrackCount; i++)
+            {
+                videoPlayer.SetDirectAudioMute(i, true);
+            }
+        }
+
         playList = PlayListManager.instance.GetPlaylist();
         if (playList.Videos.Count > 0)
         {
@@ -29,12 +42,10 @@ public class VideoHandler : MonoBehaviour
         if (!videoPlayer.isPlaying)
         {
             Debug.Log("//////////////////////////////////" + '\n' + "loading video index:" + videoIndex);
-
             loadVideo(videoIndex);
         }
         yield return new WaitForSeconds(0.5f);
         StartCoroutine("startReproduction");
-
     }
 
     void loadVideo(int index)
@@ -42,13 +53,12 @@ public class VideoHandler : MonoBehaviour
         if (index >= playList.Videos.Count)
         {
             index = 0;
-            videoIndex = 0;   
+            videoIndex = 0;
         }
 
-        videoPlayer.url = Application.dataPath+playList.Videos[index].videoURL;
+        videoPlayer.url = Application.dataPath + playList.Videos[index].videoURL;
         videoPlayer.Play();
 
         videoIndex++;
-
     }
 }
