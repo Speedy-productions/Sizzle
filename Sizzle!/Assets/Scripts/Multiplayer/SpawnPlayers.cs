adc118654f1d723cb00d6f607498cd15cf4abb2e
A00835838 <A00835838@tec.mx>
2025-11-26 17:33:23 -0600

    arreglopersonajemultiplayer

--- Sizzle!/Assets/Scripts/Multiplayer/SpawnPlayers.cs
+++ Sizzle!/Assets/Scripts/Multiplayer/SpawnPlayers.cs
@@ -1,36 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class SpawnPlayers : MonoBehaviourPunCallbacks
+public class SpawnPlayers : MonoBehaviour
 {
+    
     public GameObject playerPrefab;
 
-    public float minX;
+
+    public float mixX;
     public float maxX;
     public float minZ;
     public float maxZ;
 
-    // Este callback se llama cuando YA estás dentro de una room
-    public override void OnJoinedRoom()
+    private void Start()
     {
-        // Si estás en offline mode, no hagas nada (singleplayer)
-        if (PhotonNetwork.OfflineMode)
-            return;
-
-        // Buscar el player de singleplayer en la escena y destruirlo
-        GameObject existingPlayer = GameObject.Find("Player");
-        if (existingPlayer != null)
+        
+        if (PhotonNetwork.IsConnected && !PhotonNetwork.OfflineMode)
         {
-            Destroy(existingPlayer);   // mejor que SetActive(false) para que no estorbe
+            GameObject existingPlayer = GameObject.Find("Player");
+            if (existingPlayer)
+            {
+                existingPlayer.SetActive(false);
+            }
+
+            Vector3 randomPosition = new Vector3(Random.Range(mixX, maxX), transform.position.y, Random.Range(minZ, maxZ));
+            PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
         }
-
-        // Spawnear el player de multijugador
-        Vector3 randomPosition = new Vector3(
-            Random.Range(minX, maxX),
-            transform.position.y,
-            Random.Range(minZ, maxZ)
-        );
-
-        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
     }
+
+
 }
